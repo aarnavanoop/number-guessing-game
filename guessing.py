64cbada92b959ffdab7a57318177e40c8b5d2de8
aarnavanoop <aarnavanoop@gmail.com>
2024-08-10 17:59:10 +1000

    Added therough boilerplate

---
+++ guessing.py
@@ -0,0 +1,10 @@
+import random
+
+#Assign the random number to a variable
+
+#Ask user to guess the number
+
+#If number is correct exit the loop
+
+
+#If number is wrong keep within the loop, asking them to guess again
